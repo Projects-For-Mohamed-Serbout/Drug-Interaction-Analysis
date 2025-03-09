f093132f87b8215bffbf32cbc94720a3e82bab5c
Mohamed Serbout <m.serbout7@outlook.com>
2025-03-09 18:08:27 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
-"""Setup script for the Python project."""
-
 from setuptools import setup, find_packages
 
 setup(
-    name='my_python_project',
+    name='Drug-Interaction-Analysis',
     version='1.0.0',
-    description='A sample Python project',
-    author='Your Name',
-    author_email='your.email@example.com',
+    description='Efficient Drug Interaction Analysis using NoSQL Databases and NLP Techniques for Enhanced Patient Safety',
+    author='Mohamed Serbout',
+    author_email='m.serbout7@outlook.com',
     packages=find_packages(),
     install_requires=[
         'pylint',
         'pytest'
     ],
     python_requires='>=3.9',
-    entry_points={
-        'console_scripts': [
-            'my_command=my_package.module:main_function'
-        ]
-    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',

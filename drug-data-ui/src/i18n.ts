c1b77c745e1ec594b4ea90471c8d2fc37126ed22
Mohamed Serbout <m.serbout7@outlook.com>
2025-05-12 01:40:41 +0200

    Update ci.yml and i18n

--- drug-data-ui/src/i18n.ts
+++ drug-data-ui/src/i18n.ts
@@ -2,35 +2,36 @@ import i18n from 'i18next'
 import Cache from 'i18next-localstorage-backend'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import i18n_en from './i18n/en.json'
-import i18n_es from './i18n/es.json'
+
+import i18n_en from './i18n/en.json';
+import i18n_es from './i18n/es.json';
 
 const resources = {
   en: {
-    translation: i18n_en
+    translation: i18n_en,
   },
   es: {
-    translation: i18n_es
-  }
-}
+    translation: i18n_es,
+  },
+};
 
-const options = {
-  order: ['indexedDB', 'localStorage', 'navigator'],
+const detectionOptions = {
+  order: ['localStorage', 'navigator'],
   caches: ['localStorage'],
-  lookupQuerystring: 'lng'
-}
+  lookupQuerystring: 'lng',
+};
 
 void i18n
-  .use(Cache)
+  .use(Cache) // Only uses localStorage
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    detection: options,
+    detection: detectionOptions,
     resources,
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false
-    }
-  })
+      escapeValue: false,
+    },
+  });
 
-export default i18n
+export default i18n;

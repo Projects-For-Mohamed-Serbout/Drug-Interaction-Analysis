4f68747fcfeea0c90fb94b05c891f518051c423d
Mohamed Serbout <m.serbout7@outlook.com>
2025-05-12 01:38:58 +0200

    Add drug-data-ui with configuration

---
+++ drug-data-ui/src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+function App() {
+  return (
+    <div className="text-center mt-10 text-2xl font-bold">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
---
+++ drug-data-ui/src/i18n.ts
@@ -0,0 +1,36 @@
+import i18n from 'i18next'
+import Cache from 'i18next-localstorage-backend'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import i18n_en from './i18n/en.json'
+import i18n_es from './i18n/es.json'
+
+const resources = {
+  en: {
+    translation: i18n_en
+  },
+  es: {
+    translation: i18n_es
+  }
+}
+
+const options = {
+  order: ['indexedDB', 'localStorage', 'navigator'],
+  caches: ['localStorage'],
+  lookupQuerystring: 'lng'
+}
+
+void i18n
+  .use(Cache)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    detection: options,
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false
+    }
+  })
+
+export default i18n
